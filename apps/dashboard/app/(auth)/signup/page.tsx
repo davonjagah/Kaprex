17ffab6f8cf9c9b86db6b5068fb9beebd3956b57
Ademola Madelewi <ademolamadelewi@gmail.com>
2025-07-04 01:25:07 +0100

    feat(dashboard): fix build error

--- apps/dashboard/app/(auth)/signup/page.tsx
+++ apps/dashboard/app/(auth)/signup/page.tsx
@@ -1,11 +1,11 @@
 import { redirect } from "next/navigation";
 import SignupForm from "../../../components/Auth/SignUpForm/SignupForm";
 
-interface SignupPageProps {
-  searchParams: { accountType?: string };
-}
-
-const SignupPage = async ({ searchParams }: SignupPageProps) => {
+const SignupPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ accountType?: string }>;
+}) => {
   const accountType = (await searchParams).accountType;
 
   if (!accountType || !["individual", "business"].includes(accountType)) {
